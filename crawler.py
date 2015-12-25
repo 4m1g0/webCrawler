97cc9c84e9c02243c682e48951940dc450da001d
4m1g0 <dev.4m1g0@gmail.com>
2015-12-25 02:19:14 +0100

    Handle keyboard interupt, save all data and finish

--- crawler.py
+++ crawler.py
@@ -51,29 +51,36 @@ def main(argv):
     except:
         catalog = {}
     
-    uri = 'http://www.banggood.com/'
-    html = urlopen(uri).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    categories_bulk = soup.find_all('a')
-    # todas las categorias existentes
-    categories = [x for x in categories_bulk if isCategoryUrl(x.get('href'))]
-    
-    # recorremos todos los items de categoria (paginados)
-    for uri in categories:
-        while (uri):
-            print("DEBUG: " + str(uri))
-            try:
-                html = urlopen(uri.get('href')).read()
-            except:
-                time.sleep(5) # wait 5 seconds to retry
-                print("Error page: " + uri.get('href') + ". Retrying...")
-                continue # retry same url (infinite loop until page is on)
-            soup = BeautifulSoup(html, 'html.parser')
-            items = getItems(soup)
-            addItems(catalog, items)
-            uri = soup.find('a', title='Next page')
-        print(len(catalog))
-        time.sleep(20) 
+    try:
+        uri = 'http://www.banggood.com/'
+        html = urlopen(uri).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        categories_bulk = soup.find_all('a')
+        # todas las categorias existentes
+        categories = [x for x in categories_bulk if isCategoryUrl(x.get('href'))]
+        
+        # recorremos todos los items de categoria (paginados)
+        for uri in categories:
+            while (uri):
+                print("DEBUG: " + str(uri))
+                try:
+                    html = urlopen(uri.get('href')).read()
+                except:
+                    time.sleep(5) # wait 5 seconds to retry
+                    print("Error page: " + uri.get('href') + ". Retrying...")
+                    continue # retry same url (infinite loop until page is on)
+                soup = BeautifulSoup(html, 'html.parser')
+                items = getItems(soup)
+                addItems(catalog, items)
+                uri = soup.find('a', title='Next page')
+            print(len(catalog))
+            time.sleep(20) 
+            break
+            
+    except:
+        pickle.dump(catalog, open(argv[0], 'wb'))
+        sys.exit(0)
+        
     pickle.dump(catalog, open(argv[0], 'wb'))
 
 if __name__ == "__main__":
